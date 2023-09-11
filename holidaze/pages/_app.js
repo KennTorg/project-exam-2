803e20c37c2a8b0ca9ca2555498c8f0ca7a01e9d
Kenn Torg <kennettorgersenmail@gmail.com>
2023-09-11 22:40:21 +0200

    updated index and app

--- holidaze/pages/_app.js
+++ holidaze/pages/_app.js
@@ -1,5 +1,11 @@
-import '@/styles/globals.css'
+import "@/styles/globals.scss";
+import { UserProvider } from "@/context/UserContext";
+import "@fortawesome/fontawesome-svg-core/styles.css";
 
 export default function App({ Component, pageProps }) {
-  return <Component {...pageProps} />
+  return (
+    <UserProvider>
+      <Component {...pageProps} />
+    </UserProvider>
+  );
 }
