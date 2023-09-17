a2b57f9991d23088733832d88ef91d84f7e571b7
Kenn Torg <kennettorgersenmail@gmail.com>
2023-09-17 23:23:02 +0200

    calender

--- holidaze/components/VenueCalendar/index.js
+++ holidaze/components/VenueCalendar/index.js
@@ -3,14 +3,29 @@ import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
 import styles from "./VenueCalender.module.scss";
 
-const VenueCalendar = ({ availableDates }) => {
+const VenueCalendar = ({ bookedDates }) => {
   return (
     <div className={styles.calendar}>
       <Calendar
-        tileClassName={({ date }) => {
-          // Check if the date is in the availableDates array
-          const isAvailable = availableDates.includes(date.toISOString());
-          return isAvailable ? "custom-class" : ""; // Apply custom class if date is available
+        tileContent={({ date, view }) => {
+          // Check if bookedDates is defined
+          if (!bookedDates) {
+            return null;
+          }
+
+          // Check if the date is within a booked range
+          const isBooked = bookedDates.some((booking) => {
+            return (
+              date >= new Date(booking.start) && date <= new Date(booking.end)
+            );
+          });
+
+          // If it's the month view, show a custom content for booked dates
+          if (view === "month" && isBooked) {
+            return <div className='booked-day'>Booked</div>;
+          }
+
+          return null; // Return null for other cases
         }}
         value={new Date()} // Set the initial date if needed
       />
