03fd53ea6ad421edd18b6be655062e17f5ec6df5
Kenn Torg <kennettorgersenmail@gmail.com>
2023-09-19 16:57:26 +0200

    Calendar fix

--- holidaze/components/VenueCalendar/index.js
+++ holidaze/components/VenueCalendar/index.js
@@ -1,3 +1,5 @@
+// VenueCalendar.js
+
 import React from "react";
 import Calendar from "react-calendar";
 import "react-calendar/dist/Calendar.css";
@@ -7,27 +9,21 @@ const VenueCalendar = ({ bookedDates }) => {
   return (
     <div className={styles.calendar}>
       <Calendar
-        tileContent={({ date, view }) => {
+        tileDisabled={({ date }) => {
           // Check if bookedDates is defined
           if (!bookedDates) {
-            return null;
+            return false;
           }
 
           // Check if the date is within a booked range
-          const isBooked = bookedDates.some((booking) => {
+          return bookedDates.some((booking) => {
             return (
               date >= new Date(booking.start) && date <= new Date(booking.end)
             );
           });
-
-          // If it's the month view, show a custom content for booked dates
-          if (view === "month" && isBooked) {
-            return <div className='booked-day'>Booked</div>;
-          }
-
-          return null; // Return null for other cases
         }}
-        value={new Date()} // Set the initial date if needed
+        value={new Date()}
+        className={styles.customCalendar} // You can add your custom styles here
       />
     </div>
   );
