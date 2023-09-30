428af6cbe86e724d2d1c4919b8c41ca7273ba0d2
Kenn Torg <kennettorgersenmail@gmail.com>
2023-09-30 18:11:07 +0200

    JSX

--- holidaze/pages/_app.jsx
+++ holidaze/pages/_app.jsx
@@ -2,6 +2,8 @@ import "@/styles/globals.scss";
 import { UserProvider } from "@/context/UserContext";
 import { VenueProvider } from "@/context/VenueContext";
 import { config } from "@fortawesome/fontawesome-svg-core";
+import { ToastContainer } from "react-toastify";
+import "react-toastify/dist/ReactToastify.css";
 import "@fortawesome/fontawesome-svg-core/styles.css";
 config.autoAddCss = false;
 
@@ -9,6 +11,7 @@ export default function App({ Component, pageProps }) {
   return (
     <VenueProvider>
       <UserProvider>
+        <ToastContainer />
         <Component {...pageProps} />
       </UserProvider>
     </VenueProvider>
--- holidaze/pages/index.jsx
+++ holidaze/pages/index.jsx
@@ -1,5 +1,5 @@
 import Layout from "@/components/Layout";
-import UserProfile from "@/components/UserProfile";
+//import UserProfile from "@/components/UserProfile";
 import HomeContent from "@/components/Pages/HomeContent";
 //import styles from "../styles/pages/Home.module.scss";
 
@@ -7,7 +7,6 @@ export default function Home() {
   return (
     <Layout>
       <HomeContent />
-      <UserProfile />
     </Layout>
   );
 }
