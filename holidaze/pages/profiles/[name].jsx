db1e1229d4d03566f615cdeaffbc9aae32edd75b
Kenn Torg <kennettorgersenmail@gmail.com>
2023-09-30 23:48:48 +0200

    revert

--- holidaze/pages/profiles/[name].jsx
+++ holidaze/pages/profiles/[name].jsx
@@ -37,7 +37,6 @@ const UserProfile = () => {
     if (name) {
       fetchUserProfileData();
     }
-    // eslint-disable-next-line react-hooks/exhaustive-deps
   }, [name]);
 
   const handleAvatarChange = async (newAvatarUrl) => {
